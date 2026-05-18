4bf31eabca2a18d9f82d67f6e538880a69be4738
Surya Balaji <suryab@utexas.edu>
2026-05-18 16:47:09 -0500

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        char* c1 = &s[0];
+        char* c2 = &s[s.size()-1];
+
+        for(char& c:s){
+            c = tolower(c);
+        }
+
+        while(c1 < c2){
+
+            if(!isalnum(*c1)){
+                c1++;
+                continue;
+            }
+
+            if(!isalnum(*c2)){
+                c2--;
+                continue;
+            }
+
+            if(*c1 != *c2){
+                return false;
+            }
+
+            c1++;
+            c2--;
+        }
+
+        return true;
+    }
+};
