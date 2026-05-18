e3571934f8fb79e047aee38f00b687e26e348e35
Surya Balaji <suryab@utexas.edu>
2026-05-18 16:34:10 -0500

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for(int num:nums){
+            seen.insert(num);
+        }
+
+        vector<int> starts;
+
+        for(int num:nums){
+            if(!seen.contains(num - 1)){
+                starts.push_back(num);
+            }
+        }
+
+        int max = 0;
+        int curMax = 1;
+        for(int s:starts){
+            while(seen.contains(s+curMax)){
+                curMax++;
+            }
+            if(curMax > max){
+                max = curMax;
+            }
+            curMax = 1;
+        }
+
+        return max;
+    }
+};
