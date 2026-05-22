422038b492b4b0a71c3a3a032fb7e28e487d893d
Surya Balaji <suryab@utexas.edu>
2026-05-22 00:16:10 -0500

    Add: longest-substring-without-duplicates - submission-9

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-9.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.length() < 2){
+            return s.length();
+        }
+        int maxLen = 0;
+        int l=0, r=0;
+        unordered_set<char> seen;
+
+        for(int r = 0; r < s.length(); r++){
+            while(seen.contains(s[r])){
+                seen.erase(s[l]);
+                l++;
+            }
+            seen.insert(s[r]);
+            maxLen = max(maxLen, r-l+1);
+        }
+
+        return maxLen;
+    }
+};
