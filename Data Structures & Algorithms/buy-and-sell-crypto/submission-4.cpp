ef4972784faa21eb7c04952417a21d630bca194e
Surya Balaji <suryab@utexas.edu>
2026-05-21 23:31:24 -0500

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int minB = prices[0];
+        int buy = prices[0];
+        int maxP = 0;
+
+        for(int i = 1; i < prices.size(); i++){
+            maxP = max(maxP, prices[i] - minB);
+            minB = min(prices[i], minB);
+        }
+            
+        return maxP;
+    }
+};
