209de9579c6238e44a87e7d0ac90c7d944ca6404
Surya Balaji <suryab@utexas.edu>
2026-05-23 01:32:54 -0500

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()){
+            return false;
+        }
+
+        unordered_map<char,int> s1Chars;
+        for(char c:s1){
+            s1Chars[c]++;
+        }
+
+        unordered_map<char,int> seen;
+        int l = 0;
+        for(int r = 0; r < s2.length(); r++){
+            seen[s2[r]]++;
+            while(seen[s2[r]] > s1Chars[s2[r]]){
+                seen[s2[l]]--;
+                l++;
+            }
+            if(r-l+1 == s1.length()){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
